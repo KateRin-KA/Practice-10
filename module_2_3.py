9de6aabe407486ef1fc7789a43ff4946072715a9
Klyueva Ekaterina <Kuska2006@list.ru>
2024-10-07 23:20:43 +0300

    Цикл While

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,15 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-positive = []
 a = 0
 while a < len(my_list):
-    if my_list[a] <= 0:
+    number = my_list[a]
+    a = a + 1
+    if number == 0:
+        continue
+    elif number < 0:
+        print('Отрицательное число:', number)
         break
-positive.oppend(my_list[a])
-a += 1
-print(positive)
+    elif a == len(my_list):
+        print(number)
+        print('Конец списка')
+    else:
+        print(number)
